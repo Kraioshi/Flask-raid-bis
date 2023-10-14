e5ba2efa61e5a930512055c999b4c66fe946f4f3
Turgay <turgaj.abdullajev@gmail.com>
2023-10-15 01:56:08 +0400

    added anchor tags and links json

--- app.py
+++ app.py
@@ -4,10 +4,16 @@ from flask_bootstrap import Bootstrap5
 app = Flask(__name__)
 Bootstrap5(app)
 
+links = [
+    {
+        "name": "blm-2521", "url": 'https://etro.gg/gearset/1d113f03-16e3-4a47-83a9-c3366a0fff84'
+    }
+]
+
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', links=links)
 
 
 if __name__ == '__main__':
