fe4b55d9f4449d788d6934c007f351894f262841
Turgay <turgaj.abdullajev@gmail.com>
2023-10-14 19:53:05 +0400

    added p11 loot

--- static/js/script.js
+++ static/js/script.js
@@ -1,22 +1,32 @@
-// Hide kokytos loot
+// HTML divs
 var lootNine = document.querySelector('.kokytos-loot');
-lootNine.style.display = 'none';
-
 var lootTen = document.querySelector('.pandaemonium-loot');
+var lootEleven = document.querySelector('.themis-loot');
+
+// Hide by default
+lootNine.style.display = 'none';
 lootTen.style.display = 'none';
+lootEleven.style.display = 'none';
+
+// Toggle loot function
+function toggleLoot(lootElement) {
+    if (lootElement.style.display === 'none') {
+        lootElement.style.display = 'block';
+    } else {
+        lootElement.style.display = 'none';
+    }
+}
+
+// Call functions
 
 document.getElementById('kokytos').addEventListener('click', function() {
-    if (lootNine.style.display === 'none') {
-        lootNine.style.display = 'block';
-    } else {
-        lootNine.style.display = 'none';
-    }
+    toggleLoot(lootNine);
 });
 
 document.getElementById('pandaemonium').addEventListener('click', function() {
-    if (lootTen.style.display === 'none') {
-        lootTen.style.display = 'block';
-    } else {
-        lootTen.style.display = 'none';
-    }
+    toggleLoot(lootTen);
+});
+
+document.getElementById('themis').addEventListener('click', function() {
+    toggleLoot(lootEleven);
 });
